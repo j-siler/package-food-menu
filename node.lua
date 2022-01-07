4c4de1d5fdcc3d4f5d444524a8252c677a34bbdd
Jim Siler <j.siler@jimsiler.com>
2022-01-07 18:23:55 -0500

    debugging

--- node.lua
+++ node.lua
@@ -62,6 +62,7 @@ local statusline = string.format(
     --     sys.displays[2].y2)
     --   statusline = statusline + statusline2
     -- end
+    statusline = statusline + "!!!"
     font:write(0, 0, statusline, font_size*1.0, 0, 255, 255, 1.0)
 
     local y = 50
