5451aec45c0fbe7389e3c639ef953dbca75c0aa0
Jim Siler <j.siler@jimsiler.com>
2022-01-07 13:01:10 -0500

    Testing sys.displays as per comment by FW

--- node.lua
+++ node.lua
@@ -40,8 +40,8 @@ end)
 function node.render()
     -- background.draw(0, 0, WIDTH, HEIGHT, .8)
     background.draw(
-        sys.displays[2].x1, sys.displays[2].y1,
-        sys.displays[2].x2, sys.displays[2].y2
+        sys.displays[1].x1, sys.displays[1].y1,
+        sys.displays[1].x2, sys.displays[1].y2
     )
     local statusline = string.format("W=%d H=%d", WIDTH, HEIGHT)
     font:write(0, 0, statusline, font_size*1.2, 255, 255, 0, 1.0)
