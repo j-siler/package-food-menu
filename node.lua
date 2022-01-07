cd4d8b1108b97eaa03ec5236d708962bf65fbea3
Jim Siler <j.siler@jimsiler.com>
2022-01-07 15:21:41 -0500

    debugging

--- node.lua
+++ node.lua
@@ -50,7 +50,7 @@ function node.render()
       sys.displays[1].y1,                                                           
       sys.displays[1].y2)                                                           
     font:write(0, 0, statusline, font_size*1.0, 0, 255, 255, 1.0)                   
-    local statusline = string.format(statusformat2,                                 
+    statusline = string.format(statusformat2,                                 
       sys.displays[2].x1,                                                           
       sys.displays[2].x2,                                                           
       sys.displays[2].y1,                                                           
