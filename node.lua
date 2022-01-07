1c736b6a3bd3aed541bb7b0ce4fb415eb912973d
Jim Siler <j.siler@jimsiler.com>
2022-01-07 16:53:51 -0500

    debugging

--- node.lua
+++ node.lua
@@ -39,28 +39,29 @@ end)
 
 function node.render()
     background.draw(0, 0, WIDTH, HEIGHT, 1.0)
-    local statusformat  = "H=%d W=%d %d screens.  Screen 1 x1=%d x2=%d, y1=%d y2=%d"  
-    local statusline = string.format(statusformat,                                  
-      HEIGHT,                                                                       
+    local statusformat  = "H=%d W=%d %d screens.  Screen 1 x1=%d x2=%d, y1=%d y2=%d "
+
+    local statusline = string.format(statusformat,
+      HEIGHT,
       WIDTH,
       #sys.displays,
-      sys.displays[1].x1,                                                           
-      sys.displays[1].x2,                                                           
-      sys.displays[1].y1,                                                           
-      sys.displays[1].y2)                                                        
-    font:write(0, 0, statusline, font_size*1.0, 0, 255, 255, 1.0)                   
+      sys.displays[1].x1,
+      sys.displays[1].x2,
+      sys.displays[1].y1,
+      sys.displays[1].y2)
 
-    if sys.displays[2] == nil then 
-      font:write(0, 20, "No secondary monitor", font_size*1.0, 0, 255, 255, 1.0)
+    if sys.displays[2] == nil then
+      statusline = statusline + "No secondary screen"
     else
-      local statusformat2  = "        d[1].x1=%d, d[1].x2=%d, d[1].y1=%d, d[1].y2=%d"  
-      local statusline2 = string.format(statusformat2,                                  
-        sys.displays[2].x1,                                                           
-        sys.displays[2].x2,                                                           
-        sys.displays[2].y1,                                                           
-        sys.displays[2].y2)                                                           
-      font:write(0, 20, statusline2, font_size*1.0, 0, 255, 255, 1.0)
+      local statusformat2  = "Screen 2 d[1].x1=%d, d[1].x2=%d, d[1].y1=%d, d[1].y2=%d"
+      local statusline2 = string.format(statusformat2,
+        sys.displays[2].x1,
+        sys.displays[2].x2,
+        sys.displays[2].y1,
+        sys.displays[2].y2)
+      statusline = statusline + statusline2
     end
+    font:write(0, 0, statusline, font_size*1.0, 0, 255, 255, 1.0)
 
     local y = 50
     for idx, item in ipairs(items) do
