bd90ac02a0f8884d5b2a9f7b85883291b9d19de9
Jim Siler <j.siler@jimsiler.com>
2022-01-09 20:43:09 -0500

    debugging

--- node.lua
+++ node.lua
@@ -47,9 +47,8 @@ function node.render()
       sys.displays[1].y1,
       sys.displays[1].y2)
    font:write(0, 20, statusline, font_size*1.0, 0, 255, 255, 1.0)
-
-   statusformat  = "HDMI-1: x1=%d x2=%d y1=%d y2=%d"
    if sys.displays[2] ~= nil then
+      statusformat  = "HDMI-1: x1=%d x2=%d y1=%d y2=%d"
       statusline = string.format(
 	 statusformat,
 	 sys.displays[2].x1,
@@ -57,6 +56,7 @@ function node.render()
 	 sys.displays[2].y1,
 	 sys.displays[2].y2)
       font:write(0, 0, statusline, font_size*1.0, 0, 255, 255, 1.0)
+   end
 --       statusformat  = "HDMI-1: x1=%d x2=%d y1=%d y2=%d"
 --       statusline = string.format(statusformat,
 -- 				 sys.displays[2].x1,
